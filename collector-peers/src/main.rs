2a88c20e311d0b94d746caac96741ac0c5984990
Mingwei Zhang <mingwei@mwzhang.com>
2021-11-29 13:16:05 -0800

    fix collector/peer bgp-id rendering

--- collector-peers/src/main.rs
+++ collector-peers/src/main.rs
@@ -3,6 +3,7 @@ use std::fs::File;
 use serde::{Serialize, Deserialize};
 use serde_json::{Value, json};
 use rayon::prelude::*;
+use log::info;
 use bgp_models::prelude::*;
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -21,6 +22,9 @@ struct LatestData {
     timestamp: u64,
 }
 fn main() {
+    env_logger::init();
+
+    info!("getting list of most recent rib dumps from bgpkit broker...");
     let latest_files: Latest = reqwest::blocking::get("http://api.broker.bgpkit.com/v1/meta/latest_times").unwrap().json().unwrap();
     let mut ribs = vec![];
     for data in latest_files.data {
@@ -28,8 +32,11 @@ fn main() {
             ribs.push((data.collector_id.clone(), data.item_url.clone()));
         }
     }
+    info!("total of {} rib dumps found.", ribs.len());
+
+    info!("start parsing peer index table from all the rib dump files...");
     let collect_peers: Vec<(String, HashMap<u32, Peer>)> = ribs.par_iter().filter_map(|(collector,url)| {
-        dbg!(&url);
+        info!("start parsing peer index table from {}", &url);
         let parser = bgpkit_parser::BgpkitParser::new(url).unwrap();
         let mrt_record = parser.into_record_iter().next().unwrap();
         match mrt_record.message{
@@ -44,8 +51,8 @@ fn main() {
             _ => { None }
         }
     }).collect();
-
+    info!("serializing to local json file...");
     let collect_peers_map: HashMap<String, HashMap<u32, Peer>> = HashMap::from_iter(collect_peers);
-
-    serde_json::to_writer_pretty(&File::create("collector_peers.json").unwrap(), &json!(collect_peers_map));
+    serde_json::to_writer_pretty(&File::create("collector_peers.json").unwrap(), &json!(collect_peers_map)).unwrap();
+    info!("all done!");
 }
